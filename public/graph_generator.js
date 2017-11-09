940c54d8454f2d6f144a8f96771746eec23d82fb
Brett Bushnell <bbushnell@email.arizona.edu>
2017-11-09 03:41:00 +0000

    added hour formatiing to horizontal axis

--- public/graph_generator.js
+++ public/graph_generator.js
@@ -3,21 +3,25 @@ google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ['Hour', 'UV Exposure'],
-    [ 12,      1],
-    [ 13,      2],
-    [ 14,     3],
-    [ 15,      4],
-    [ 16,      5],
-    [ 17,    6]
-  ]);
+  var data = new google.visualization.DataTable();
+  data.addColumn('timeofday', 'Hour');
+  data.addColumn('number', 'UV Level');
 
+  data.addRows([
+    [ [12,0,0], 11],
+    [ [13,0,0], 5],
+    [ [14,0,0], 3],
+    [ [15,0,0], 7],
+    [ [16,45,0], 9],
+    [ [17,0,0], 1]
+  ]);
+  
   var options = {
     title: 'Hourly UV Exposure',
-    hAxis: {title: 'Time', minValue: 0, maxValue: 23},
-    vAxis: {title: 'UV Level', minValue: 1, maxValue: 11},
-    legend: 'none'
+    hAxis: {title: 'Time'},
+    vAxis: {title: 'UV Level'},
+    legend: 'none',
+    lineWidth: 3
   };
 
   var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
