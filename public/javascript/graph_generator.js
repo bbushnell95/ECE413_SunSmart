c66a4ec0ae3d9003c7c46e97a2ad1bf657c698e7
Brett Bushnell <bbushnell@email.arizona.edu>
2017-11-18 04:27:55 +0000

    Modified the HTML and graph generator to be prepared for real data.

--- public/javascript/graph_generator.js
+++ public/javascript/graph_generator.js
@@ -4,17 +4,20 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
   var data = new google.visualization.DataTable();
-  data.addColumn('timeofday', 'Hour');
+  data.addColumn('datetime', 'Hour');
   data.addColumn('number', 'UV Level');
+  var rows = [];
 
-  data.addRows([
-    [ [12,0,0], 11],
-    [ [13,0,0], 5],
-    [ [14,0,0], 3],
-    [ [15,0,0], 7],
-    [ [16,45,0], 9],
-    [ [17,0,0], 1]
-  ]);
+  $.get("http://ec2-13-58-73-63.us-east-2.compute.amazonaws.com:3000/data/1a0040000d51353532343635",null, function(response){
+    var samples = response.samples;
+    for(var i = 0; i < samples.length; i++){
+        rows.push([new Date(samples[i].timeStamp*1000), samples[i].uvIndex]);
+    }
+    
+  }, "json");
+    
+
+  data.addRows(rows);
   
   var options = {
     title: 'Hourly UV Exposure',
