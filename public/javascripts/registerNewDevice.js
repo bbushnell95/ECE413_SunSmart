4b66250cc25b6eef72c4040057639229c6376042
bbushnell95 <brettbushnell95@gmail.com>
2017-12-08 21:45:32 +0000

    Front end register device stuff

---
+++ public/javascripts/registerNewDevice.js
@@ -0,0 +1,23 @@
+// Function to register a new device
+function registerDevice(){
+    $.ajax({
+        url: '/devices/register',
+        type: 'POST',
+        data: {deviceId: $("#deviceId").val()},
+        responseType: 'json',
+        success: deviceRegistered,
+        error: function(jqXHR, status, error) {
+            var response = JSON.parse(jqXHR.responseText);
+            $("#message").html("Error: " + response.message);
+            $("#message").show();
+        }
+    });
+}
+
+// Device successfully register. Update the list of devices and hide the add device form
+function deviceRegistered(data, status, xhr) {
+    // Add new device to the device list
+    $("#message").html("<li class='collection-item'>ID: " +
+            $("#deviceId").val() + ", APIKEY: " + data["apikey"] + "</li>")
+    $("#message").show()
+}
