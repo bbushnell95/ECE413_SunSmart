c4e13b83e35358d295701746d3b4a5fb3d695174
Brett Bushnell <bbushnell@email.arizona.edu>
2017-12-10 05:56:15 +0000

    Fixed up stuff to show everything off the bat

--- public/javascripts/graph_generator.js
+++ public/javascripts/graph_generator.js
@@ -9,18 +9,19 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-
-$.ajax({
-    url: '/devices/getDeviceID',
-    type: 'GET',
-    responseType:'json',
-    success: drawChart,
-    error: function(jqXHR, status, error){
-        var response = JSON.parse(jqXHR.responseText);
-        $("#chart_div").html("Error: " + response.message);
-        $("#chart_div").show();
-    }
-});
+function getDeviceID(){
+  $.ajax({
+      url: '/devices/getDeviceID',
+      type: 'GET',
+      responseType:'json',
+      success: drawChart,
+      error: function(jqXHR, status, error){
+          var response = JSON.parse(jqXHR.responseText);
+          $("#chart_div").html("Error: " + response.message);
+          $("#chart_div").show();
+      }
+  });
+};
 
 function drawChart(data, status, xhr) {
   var data = new google.visualization.DataTable();
@@ -76,23 +77,6 @@ function drawChart(data, status, xhr) {
     var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
   
     chart.draw(data, options);
-    
-    var rangeChoice = document.createElement("select");
-    rangeChoice.innerHTML = "<option value=\"hour\">Last Hour</option><option value=\"day\">Last Day</option><option value=\"week\">Last Week</option><option value=\"month\">Last Month</option><option value=\"year\">Last Year</option>";
-    rangeChoice.setAttribute("id", "rangeSelect");
-    
-    var refreshButton = document.createElement("button");
-    refreshButton.setAttribute("type", "button");
-    refreshButton.addEventListener("click", function() {
-      var range = document.getElementById("rangeSelect").options[document.getElementById("rangeSelect").selectedIndex].value;
-      document.getElementById("chart_div").innerHTML = "";
-      drawChart(range);
-    });
-    document.getElementById("chart_div").appendChild(rangeChoice);
-    document.getElementById("chart_div").appendChild(refreshButton);
-    
-    
-  }, "json");
-    
-
 }
+
+$("#refresh").click(getDeviceID);
