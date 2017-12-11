a746ee0c845fe29ae7b6e0762612de3ac4c59abf
Brett Bushnell <brettbushnell95@gmail.com>
2017-12-10 17:58:52 -0700

    Adding the api_endpoints_andSchemas.pdf

--- models/data.js
+++ models/data.js
@@ -3,6 +3,8 @@ var db = require("../db");
 var dataSchema = new db.Schema({
 	deviceId: String,
 	uvIndex: Number,
+	longitude: Number,
+	latitude: Number,
 	timeStamp: Number //{type: Date, default: Date.now }
 });
 
